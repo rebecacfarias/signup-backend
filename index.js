0b9166f09b389694aa5eae3bf63b8b73cac789aa
rebecacfarias <fariasrebecac@gmail.com>
2023-05-30 00:52:32 -0300

    deploy adjustments

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/users', userRoutes)
 
 
 db.connect(() => {
-    app.listen(process.env.SERVER_PORT, () => {
+    app.listen(process.env.PORT, () => {
         console.log('Server Started')
     })
 })
