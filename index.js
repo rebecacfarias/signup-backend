272fce0e6b77b5692e2eb75b229b0b3f906a75a1
rebecacfarias <fariasrebecac@gmail.com>
2023-05-30 21:54:35 -0300

    testing without using cors

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
 const userRoutes = require('./routes/userRoutes')
 const db = require('./db')
 
 const app = express()
 
 app.use(express.json())
-
-app.use(cors())
   
 app.use('/users', userRoutes)
 
