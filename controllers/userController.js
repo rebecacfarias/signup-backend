8c472217e7c9b6d4e7167984cf62c6944ba7749d
rebecacfarias <fariasrebecac@gmail.com>
2023-05-31 10:27:23 -0300

    adding logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,10 @@ async function addNewUser(req, res){
         .then((response)=> {
             res.setHeader('Access-Control-Max-Age', '3600')
             res.status(201).json(response)})
-        .catch((error)=> res.status(error.status || 500).json({message: error.message}))
+        .catch((error)=> {
+            console.log(error)
+            res.status(error.status || 500).json({message: error.message})
+        })
 }
 
 async function updateUser(req, res){
